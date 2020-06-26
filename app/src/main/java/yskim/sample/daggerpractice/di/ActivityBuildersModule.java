de2b39ed66f684c7d51763b7e8ae61fa57f4be61
김윤수 <yskim@taeha.co.kr>
2020-06-26 16:24:15 +0900

    @ContributesAndroidInjector

---
+++ app/src/main/java/yskim/sample/daggerpractice/di/ActivityBuildersModule.java
@@ -0,0 +1,18 @@
+package yskim.sample.daggerpractice.di;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
+import yskim.sample.daggerpractice.AuthActivity;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+
+    @Provides
+    static String someString() {
+        return "this is a test string";
+    }
+}
--- app/src/main/java/yskim/sample/daggerpractice/di/AppComponent.java
+++ app/src/main/java/yskim/sample/daggerpractice/di/AppComponent.java
@@ -6,15 +6,18 @@ import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
+import yskim.sample.daggerpractice.AuthActivity;
 import yskim.sample.daggerpractice.BaseApplication;
 
 @Component(
         modules = {
                 AndroidSupportInjectionModule.class,
-
+                ActivityBuildersModule.class,
         }
 )
 public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+//    void inject(AuthActivity activity);
 
     @Component.Builder
     interface Builder {
