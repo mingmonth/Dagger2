b08d7ace7b418618403406abca6492171d6cee35
김윤수 <yskim@taeha.co.kr>
2020-06-29 19:01:03 +0900

    Managing Authentication with Dagger (part 2/3)

--- app/src/main/java/yskim/sample/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/yskim/sample/daggerpractice/ui/auth/AuthViewModel.java
@@ -14,6 +14,7 @@ import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import yskim.sample.daggerpractice.SessionManager;
 import yskim.sample.daggerpractice.di.models.User;
 import yskim.sample.daggerpractice.di.network.auth.AuthApi;
 
@@ -22,12 +23,17 @@ public class AuthViewModel extends ViewModel {
     // logt
     private static final String TAG = "AuthViewModel";
 
-    private MediatorLiveData<AuthResource<User>> authUser = new MediatorLiveData<>();
+//    private MediatorLiveData<AuthResource<User>> authUser = new MediatorLiveData<>();
+    private SessionManager sessionManager;
 
     private final AuthApi authApi;
     @Inject
-    public AuthViewModel(AuthApi authApi) {
+    public AuthViewModel(
+            AuthApi authApi,
+            SessionManager sessionManager
+            ) {
         this.authApi = authApi;
+        this.sessionManager = sessionManager;
         Log.d(TAG, "AuthViewModel: viewModel is working...");
 
 //        authApi.getUser(1)
@@ -65,44 +71,53 @@ public class AuthViewModel extends ViewModel {
 
     public void authenticateWithId(int userId) {
 
-        authUser.setValue(AuthResource.loading((User)null));
+        Log.d(TAG, "authenticateWithId: attempting to login.");
+        sessionManager.authenticateWithId(queryUserId(userId));
 
-        final LiveData<AuthResource<User>> source = LiveDataReactiveStreams.fromPublisher(
+//        authUser.setValue(AuthResource.loading((User)null));
+
+//        final LiveData<AuthResource<User>> source =
+//        );
+
+//        authUser.addSource(source, new Observer<AuthResource<User>>() {
+//            @Override
+//            public void onChanged(AuthResource<User> user) {
+//                authUser.setValue(user);
+//                authUser.removeSource(source);
+//            }
+//        });
+    }
+
+    private LiveData<AuthResource<User>> queryUserId(int userId) {
+        return LiveDataReactiveStreams.fromPublisher(
                 authApi.getUser(userId)
 
-                        // instead of calling onError(error happens)
-                        .onErrorReturn(new Function<Throwable, User>() {
-                            @Override
-                            public User apply(Throwable throwable) throws Exception {
-                                User errorUser = new User();
-                                errorUser.setId(-1);
-                                return errorUser;
+                    // instead of calling onError(error happens)
+                    .onErrorReturn(new Function<Throwable, User>() {
+                        @Override
+                        public User apply(Throwable throwable) throws Exception {
+                            User errorUser = new User();
+                            errorUser.setId(-1);
+                            return errorUser;
+                        }
+                    })
+                    // wrap User object in AuthResource
+                    .map(new Function<User, AuthResource<User>>() {
+                        @Override
+                        public AuthResource<User> apply(User user) throws Exception {
+                            if(user.getId() == -1) {
+                                return AuthResource.error("Could not authenticate", (User)null);
                             }
-                        })
-
-                        .map(new Function<User, AuthResource<User>>() {
-                            @Override
-                            public AuthResource<User> apply(User user) throws Exception {
-                                if(user.getId() == -1) {
-                                    return AuthResource.error("Could not authenticate", (User)null);
-                                }
-                                return AuthResource.authenticated(user);
-                            }
-                        })
-
-                .subscribeOn(Schedulers.io())
-        );
-
-        authUser.addSource(source, new Observer<AuthResource<User>>() {
-            @Override
-            public void onChanged(AuthResource<User> user) {
-                authUser.setValue(user);
-                authUser.removeSource(source);
-            }
-        });
+                            return AuthResource.authenticated(user);
+                        }
+                    })
+                    .subscribeOn(Schedulers.io()));
     }
 
-    public LiveData<AuthResource<User>> observeUser() {
-        return authUser;
+    public LiveData<AuthResource<User>> observeAuthState() {
+        return sessionManager.getAuthUser();
     }
+//    public LiveData<AuthResource<User>> observeUser() {
+//        return authUser;
+//    }
 }
