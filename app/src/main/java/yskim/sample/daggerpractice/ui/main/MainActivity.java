e60b04a407f6f2a42bcf106596bffe35ef68ab4b
김윤수 <yskim@taeha.co.kr>
2020-06-30 07:37:41 +0900

    Toolbar Menu and Logout

--- app/src/main/java/yskim/sample/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/yskim/sample/daggerpractice/ui/main/MainActivity.java
@@ -1,8 +1,12 @@
 package yskim.sample.daggerpractice.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import yskim.sample.daggerpractice.BaseActivity;
@@ -17,5 +21,25 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "MainActivity is called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.logout: {
+                sessionManager.logOut();
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
