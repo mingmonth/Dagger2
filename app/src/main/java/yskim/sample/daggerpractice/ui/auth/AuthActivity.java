c114615514d8cae567b8471da2159954eddd9640
김윤수 <yskim@taeha.co.kr>
2020-06-29 17:02:07 +0900

    Authentication Network Resource Class

--- app/src/main/java/yskim/sample/daggerpractice/ui/auth/AuthActivity.java
+++ app/src/main/java/yskim/sample/daggerpractice/ui/auth/AuthActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -28,6 +30,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
     private AuthViewModel viewModel;
 
     private EditText userId;
+    private ProgressBar progressBar;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -43,6 +46,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         userId = findViewById(R.id.user_id_input);
+        progressBar = findViewById(R.id.progress_bar);
 
         findViewById(R.id.login_button).setOnClickListener(this);
 
@@ -54,15 +58,47 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
         subscribeObservers();
     }
 
+    private void showProgressBar(boolean isVisible) {
+        if(isVisible) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
     private void subscribeObservers() {
-        viewModel.observeUser().observe(this, new Observer<User>() {
+        viewModel.observeUser().observe(this, new Observer<AuthResource<User>>() {
             @Override
-            public void onChanged(User user) {
-                if(user != null) {
-                    Log.d(TAG, "onChanged: " + user.getEmail());
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if(userAuthResource != null) {
+                    switch(userAuthResource.status) {
+                        case LOADING:
+                            showProgressBar(true);
+                            break;
+                        case AUTHENTICATED:
+                            showProgressBar(false);
+                            Log.d(TAG, "onChanged: LOGIN SUCCESS: " + userAuthResource.data.getEmail());
+                            break;
+                        case ERROR:
+                            showProgressBar(false);
+                            Toast.makeText(AuthActivity.this, userAuthResource.message
+                                    + "\nDid you enter a number between 1 and 10?", Toast.LENGTH_SHORT).show();
+                            break;
+                        case NOT_AUTHENTICATED:
+                            showProgressBar(false);
+                            break;
+                    }
                 }
             }
         });
+//        viewModel.observeUser().observe(this, new Observer<User>() {
+//            @Override
+//            public void onChanged(User user) {
+//                if(user != null) {
+//                    Log.d(TAG, "onChanged: " + user.getEmail());
+//                }
+//            }
+//        });
     }
 
     private void setLogo() {
