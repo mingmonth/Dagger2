1106ef1c7b6e7b1f16d42593e3cbf6e5ca14cde5
김윤수 <yskim@taeha.co.kr>
2020-06-30 15:25:14 +0900

    RxJava Query in ViewModel with Subcomponent Dependency

--- app/src/main/java/yskim/sample/daggerpractice/ui/main/posts/PostsFragment.java
+++ app/src/main/java/yskim/sample/daggerpractice/ui/main/posts/PostsFragment.java
@@ -1,19 +1,25 @@
 package yskim.sample.daggerpractice.ui.main.posts;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
 import yskim.sample.daggerpractice.R;
+import yskim.sample.daggerpractice.di.models.Post;
+import yskim.sample.daggerpractice.ui.main.Resource;
 import yskim.sample.daggerpractice.viewmodels.ViewModelProviderFactory;
 
 public class PostsFragment extends DaggerFragment {
@@ -37,5 +43,18 @@ public class PostsFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         recyclerView = view.findViewById(R.id.recycler_view);
         viewModel = new ViewModelProvider(this, providerFactory).get(PostsViewModel.class);
+        subscribeObservers();
+    }
+
+    private void subscribeObservers() {
+        viewModel.observePosts().removeObservers(getViewLifecycleOwner());
+        viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<Resource<List<Post>>>() {
+            @Override
+            public void onChanged(Resource<List<Post>> listResource) {
+                if(listResource != null) {
+                    Log.d(TAG, "onChanged: " + listResource.data);
+                }
+            }
+        });
     }
 }
